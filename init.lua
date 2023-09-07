dda2ff39ab7ba19bd8d4999d3c3ec45ec090db4e
tanayvsoni <tanayvsoni@gmail.com>
2023-09-07 12:09:18 -0600

    Setup

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ require("tanay")
 
 -- Enables Line numbers
 vim.opt.number = true
-
+vim.opt.relativenumber = true
 -- Makes sure you are CD'ed into right directory
 vim.api.nvim_command("autocmd VimEnter * silent! lcd %:p:h")
 
@@ -10,4 +10,5 @@ vim.api.nvim_command("autocmd VimEnter * silent! lcd %:p:h")
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-    
+vim.g.loaded_matchparen = true
+vim.g.loaded_matchbracket = true
